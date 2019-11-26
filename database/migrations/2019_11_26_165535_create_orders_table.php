12448f93a21981c482c86473a935949d4417f1bc
apvvyas <apvvyas@gmail.com>
2019-11-26 23:55:35 +0530

    First Commit

---
+++ database/migrations/2019_11_26_165535_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->string('invoice_number');
+            $table->float('total_amount',8,2);
+            $table->enum('status',OrderStatus::getValues());
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('orders_customer_id_foreign');
+        Schema::dropIfExists('orders');
+    }
+}
